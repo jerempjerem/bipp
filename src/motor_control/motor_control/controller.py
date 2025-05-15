b1ddd2131cba181a0e52073f3e39412864a45d85
jerempjerem <jerem830@icloud.com>
2025-05-15 14:25:09 +0200

    ....

--- src/motor_control/motor_control/controller.py
+++ src/motor_control/motor_control/controller.py
@@ -19,9 +19,9 @@ from simple_pid import PID
 
 # Constants for robot control
 ANGLE_TOLERANCE = 0.4  # Angle tolerance in radians
-POSITION_TOLERANCE = 0.02  # Position tolerance in meters
+POSITION_TOLERANCE = 0.01  # Position tolerance in meters
 MAX_ANGULAR_SPEED = 40  # Maximum angular speed in degrees/s
-MAX_LINEAR_SPEED = 0.07  # Maximum linear speed in m/s
+MAX_LINEAR_SPEED = 0.05  # Maximum linear speed in m/s
 ODOMETRY_LOG_INTERVAL = 0.1  # Log odometry every 200ms
 
 # PID controller constants
