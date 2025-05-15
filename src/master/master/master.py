854f76d5630f896aab2408ce082ea9eb83cabfc2
jerempjerem <jerem830@icloud.com>
2025-05-15 12:36:12 +0200

    added synchro

--- src/master/master/master.py
+++ src/master/master/master.py
@@ -3,35 +3,28 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
-    
+
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+
 
 class MasterNode(Node):
     def __init__(self):
         super().__init__('master_node')
         
-        # Subscriber to motor data
-        self.motor_subscription = self.create_subscription(
-            Odometry,
-            'odom',
-            self.motor_callback,
-            10
-        )
-        
-        # Subscriber to LiDAR data
-        self.lidar_subscription = self.create_subscription(
-            LaserScan,
-            'scan',
-            self.lidar_callback,
-            10
-        )
+        # Create message_filters subscribers (not rclpy.create_subscription)
+        self.lidar_sub = Subscriber(self, LaserScan, 'scan')
+        self.odom_sub = Subscriber(self, Odometry, 'odom')
 
-    def motor_callback(self, msg):
-        self.get_logger().info(f'Received motor data: {msg}')
-        # Log or process motor data here
+        # Synchronizer (queue size 10, slop 0.1s tolerance)
+        self.ts = ApproximateTimeSynchronizer(
+            [self.lidar_sub, self.odom_sub], queue_size=10, slop=0.1
+        )
+        self.ts.registerCallback(self.synced_callback)
 
-    def lidar_callback(self, msg):
-        self.get_logger().info(f'Received LiDAR data: {msg.ranges[:5]}')  # Log first 5 ranges
-        # Log or process LiDAR data here
+    def synced_callback(self, lidar_msg, odom_msg):
+        self.get_logger().info('Synchronized callback triggered')
+        self.get_logger().info(f'LiDAR ranges (first 5): {lidar_msg.ranges[:5]}')
+        self.get_logger().info(f'Odom pose: {odom_msg.pose.pose}')
 
 
 def main(args=None):
@@ -39,7 +32,7 @@ def main(args=None):
     node = MasterNode()
     rclpy.spin(node)
     rclpy.shutdown()
-     
-     
+
+
 if __name__ == "__main__":
     main()
